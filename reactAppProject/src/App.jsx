6e53b78ab8d73a36989feb942589eb140ffb28fb
Jesus Cidoncha <jesus.cidoncha.berlanga@gmail.com>
2023-12-11 17:07:02 +0100

    added components

--- reactAppProject/src/App.jsx
+++ reactAppProject/src/App.jsx
@@ -1,5 +1,14 @@
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
 function App() {
-  return <></>;
+  return (
+    <div>
+      <NavBar>this is the navBar</NavBar>
+      <Sidebar>This is the sidebar</Sidebar>
+      <Footer>this is the footer</Footer>
+    </div>
+  );
 }
 
 export default App;
---
+++ reactAppProject/src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+function NavBar() {
+  return (
+    <nav className="navBar">
+      <div>Title text navbar</div>
+      <img src="#" alt="" />
+    </nav>
+  );
+}
+
+export default NavBar;
